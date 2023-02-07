dcec00ba78f8bb2571096abd0feb6392aac05a24
yadavdevendra <devendrayadav2022aug@cedcommerce.com>
2023-02-07 18:09:41 +0530

    first commit

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,8 +7,8 @@ import { FiDribbble } from 'react-icons/fi'
 const HeaderSocials = () => {
     return (
         <div className='header__socials'>
-            <a href='' target='_blank'><BsLinkedin /></a>
-            <a href='' target='_blank'><FaGithub /></a>
+            <a href='https://www.linkedin.com/in/devendra-yadav-9591171a4/' target='_blank'><BsLinkedin /></a>
+            <a href='https://github.com/devendra-star' target='_blank'><FaGithub /></a>
             <a href='' target='_blank'><FiDribbble /></a>
         </div>
     )
